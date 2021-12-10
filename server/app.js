2bfda79022621e01d2dcf871779a5546efb1df53
Alex Martinez <alexkmartinez77@gmail.com>
2021-12-10 10:11:43 -0800

    Installs and connects to local mongoose instance,creates route to root /, installs bodyParser middleware to parse incoming requests to server.

--- server/app.js
+++ server/app.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
+
+app.use(bodyParser.json());
+//Instructs mongoose to connect to local mongoDB instance
+mongoose.connect('mongodb://localhost/myblog', { useMongoClient: true });
+//needed for async operations
+mongoose.promise = promise;
+
+
+//routes
+app.get('/', (req, res) => {
+  res.status(200).send();
+});
 
 module.exports = app;

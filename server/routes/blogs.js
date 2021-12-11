feca631bc6f16370d280917c6297555adec8d7a5
Alex Martinez <alexkmartinez77@gmail.com>
2021-12-11 12:22:18 -0800

    Creates the api/blogs route which saves the blog and then pushes the blog into the respective users blogs array.

--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -1,32 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const Blog = require('../models/Blog');
+const User = require('../models/User');
 
 
 router.route('/')
   .get((req, res) => {
     Blog.find()
-        .then( blog => res.status(200).send(blog) );
+        .then( blog => res.status(200).send(blog) )
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
     })
   .post((req, res) => {
-    newBlog = new Blog(req.body);
-    newBlog.save()
-           .then(savedBlog => res.status(201).json(savedBlog))
-           .catch(err => { res.status(500).json({error:err}); console.log(err)});
-});
+    User.findById(req.body.author)
+        .then(user => {
+            console.log('user', user);
+            const newBlog = new Blog(req.body);
+            newBlog.author = user._id;
+            newBlog.save()
+                   .then(blog => {
+                     user.blogs.push(blog);
+                     user.save();
+                     res.status(201).json(blog)});
+        })
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
+      });
 
 router.route('/:id')
   .get((req, res) => {
     Blog.findById(req.params.id)
         .then( user =>  user ? res.status(200).json(user) : res.status(404).json({message: `User ${req.params.id} not in database.`}))
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
     })
   .put((req, res) => {
     Blog.findByIdAndUpdate(req.params.id)
         .then( blog => {res.status(200).json(blog); console.log(`User ${req.params.id} has been updated.`)})
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
     })
   .delete((req, res) => {
     Blog.findByIdAndRemove(req.params.id)
         .then( blog => {res.status(200).json(blog); console.log(`User ${req.params.id} has been deleted.`)})
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
     });
 
 /*router.get('/featured', (req,res) => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,31 +6,30 @@ router.route('/')
   .get((req, res) => {
     User.find()
         .then( users => res.status(200).json(users) )
-        .catch(err => { res.status(500).json({error:err}); console.log(err)});
-  })
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
+      })
   .post((req, res) => {
     const newUser = new User(req.body);
     newUser.save()
-           .then(savedUser => res.status(201).json(savedUser))
-           .catch(err => { res.status(500).json({error:err}); console.log(err)});
-  });
+           .then( savedUser => res.status(201).json(savedUser) )
+           .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
+      });
 
 router.route('/:id')
   .get((req, res) => {
     User.findById(req.params.id)
         .then( user =>  user ? res.status(200).json(user) : res.status(404).json({message: `User ${req.params.id} is not in the database.`}))
-        .catch(err => { res.status(500).json({error:err}); console.log(err)});
-  })
-  .post((req, res) => {
-
-    })
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
+      })
   .put((req, res) => {
     User.findByIdAndUpdate(req.params.id)
         .then( user => res.status(200).json(user))
-    })
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
+      })
   .delete((req, res) => {
     User.findByIdAndRemove(req.params.id)
         .then( user => res.status(200).json(user))
-    });
+        .catch( error => { res.status(500).json({ 'error': error }); console.log(error)});
+      });
   
 module.exports = router;
